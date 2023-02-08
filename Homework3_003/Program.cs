ea82a84df8d7ea7d7a1be3152c17ce06ca2a749b
Andrey <Andre.68.1@mail.ru>
2023-02-08 15:59:44 +0500

    product calculation

---
+++ Homework3_003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N.
+
+Console.Clear();
+System.Console.WriteLine();
+Console.Write("Введите число:   ");
+
+int number = int.Parse( Console.ReadLine() ?? "0");
+int rez=1;
+int count=1;
+
+while (count<=number)
+{
+    rez=rez*count;
+    count++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Произведение чисел от 1 до "+number+" равно "+rez);
+Console.WriteLine();
