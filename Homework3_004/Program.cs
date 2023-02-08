a043da5c7983314e3f4063c9e9164d38d44be6a5
Andrey <Andre.68.1@mail.ru>
2023-02-08 16:08:41 +0500

    positivi number cubes

---
+++ Homework3_004/Program.cs
@@ -0,0 +1,18 @@
+﻿// показать кубы чисел, заканчивающтиеся на четную цифру.
+// все четные числа в кубе дают четную цифру.
+
+Console.Clear();
+Console.Write("введите крайнее число куба -");
+int number= int.Parse(Console.ReadLine() ?? "0");
+
+int rez=0;
+int count=2;
+
+
+while (count<=number && 0<count)
+{
+    rez=count*count*count;
+    Console.WriteLine(rez);
+    count=count+2;
+}   
+Console.WriteLine();
