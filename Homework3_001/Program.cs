adb58e47ec0b60163b5f98adddf4b1059236efc4
Andrey <Andre.68.1@mail.ru>
2023-02-08 15:47:39 +0500

    cube of numbers

---
+++ Homework3_001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти куб чисел от 1 до N/
+
+Console.Clear();
+
+Console.Write("Введите число - ");
+double number=int.Parse(Console.ReadLine() ?? "0");
+double rez=Math.Pow(number,3);
+
+Console.WriteLine();
+Console.WriteLine("Куб числа - "+number+" равeн "+rez);
+Console.WriteLine();
+
