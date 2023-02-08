857f0741cbec6af8aa639f468eb360a59c4bd4d1
Andrey <Andre.68.1@mail.ru>
2023-02-08 15:53:13 +0500

    sum of digits

---
+++ Homework3_002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Подсчитать сумму чисел.
+
+Console.Clear();
+
+Console.WriteLine();
+Console.Write("Введите число от 0 до 1000 - ");
+int number=int.Parse(Console.ReadLine() ?? "0");
+int sum=0;
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sum = sum + digit;
+    number = number / 10;
+
+}
+
+Console.WriteLine();
+Console.WriteLine("Сумма цифр равна  "+sum);
+Console.WriteLine();
